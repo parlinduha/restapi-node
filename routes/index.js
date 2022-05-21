e5b4b0c1673ffeeeca2b3595329f31328be8c89a
parlinduha <dhparlin@gmail.com>
2022-05-22 01:31:11 +0700

    install dotenv dan nodemon

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/me', (req, res)=>{
+  res.json({
+    message:'Welcome back to home'
+  });
+});
+
 module.exports = router;
